d093170c68a7d4e9d9990439bcbe7dd544ce78fd
miguel michelson <miguelmichelson@gmail.com>
2013-01-01 17:05:55 -0300

    entries acceptance spec

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -31,4 +31,77 @@ describe "entries" do
     page.current_url.should match(Regexp.new("/entries"))
     Postablr::Entry.all.size.should == 1
   end
+
+  it "should publish Quote" do
+    visit "/entries/new/quote"
+    page.status_code.should == 200
+    page.find_field("entry_postable_attributes_body").should be_true
+    page.find_field("entry_postable_attributes_source").should be_true
+
+    fill_in 'entry_postable_attributes_body', :with => 'Zappa picks by Larry La Londe'
+    fill_in 'entry_postable_attributes_source', :with => 'This is one of the best albums'
+    find_button("Create Entry").click
+
+    page.current_url.should match(Regexp.new("/entries"))
+    Postablr::Entry.all.size.should == 1
+  end
+
+  it "should publish Image" do
+    visit "/entries/new/image"
+    page.status_code.should == 200
+    page.find_field("entry_postable_attributes_photo").should be_true
+    page.find_field("entry_postable_attributes_body").should be_true
+
+    fill_in 'entry_postable_attributes_body', :with => 'Zappa picks by Larry La Londe'
+    attach_file('entry_postable_attributes_photo', "#{ENGINE_RAILS_ROOT}/spec/fixtures/Chungas_Revenge.jpg")
+    find_button("Create Entry").click
+
+    page.current_url.should match(Regexp.new("/entries"))
+    Postablr::Entry.all.size.should == 1
+  end
+
+  it "should publish Audio" do
+    visit "/entries/new/audio"
+    page.status_code.should == 200
+    page.find_field("entry_postable_attributes_file").should be_true
+    page.find_field("entry_postable_attributes_title").should be_true
+    page.find_field("entry_postable_attributes_body").should be_true
+
+    fill_in 'entry_postable_attributes_body', :with => 'Zappa picks by Larry La Londe'
+    fill_in 'entry_postable_attributes_title', :with => 'Zappa picks by Larry La Londe'
+    attach_file('entry_postable_attributes_file', "#{ENGINE_RAILS_ROOT}/spec/fixtures/440hz.mp3")
+    find_button("Create Entry").click
+
+    page.current_url.should match(Regexp.new("/entries"))
+    Postablr::Entry.all.size.should == 1
+  end
+
+  it "should publish video" do
+    visit "/entries/new/video"
+    page.status_code.should == 200
+    page.find_field("entry_postable_attributes_url").should be_true
+    page.find_field("entry_postable_attributes_title").should be_true
+
+    fill_in 'entry_postable_attributes_url', :with => 'http://www.youtube.com/watch?v=c6-rgXAEwAY'
+    fill_in 'entry_postable_attributes_title', :with => 'Zappa picks by Larry La Londe'
+    find_button("Create Entry").click
+
+    page.current_url.should match(Regexp.new("/entries"))
+    Postablr::Entry.all.size.should == 1
+  end
+
+  it "should publish link" do
+    visit "/entries/new/link"
+    page.status_code.should == 200
+    page.find_field("entry_postable_attributes_url").should be_true
+    page.find_field("entry_postable_attributes_title").should be_true
+
+    fill_in 'entry_postable_attributes_url', :with => 'http://www.youtube.com/watch?v=c6-rgXAEwAY'
+    fill_in 'entry_postable_attributes_title', :with => 'Zappa picks by Larry La Londe'
+    find_button("Create Entry").click
+
+    page.current_url.should match(Regexp.new("/entries"))
+    Postablr::Entry.all.size.should == 1
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ require 'shoulda/matchers/integrations/rspec'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-#ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
+ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
